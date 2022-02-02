ee95e775eb3f805296d2a30914ecf96391555f47
corno <gitkraken.a@schraverus.com>
2022-02-02 08:54:43 +0100

    .

--- pub/data/projectTemplate/pareto/copyTemplate.sh
+++ pub/data/projectTemplate/pareto/copyTemplate.sh
@@ -3,4 +3,4 @@
 cp -R ./node_modules/pareto-ts-validator/data/projectTemplate/. .. \
 
 #npm messes with .gitignore, that's why I need to handle it separately
-cp ./node_modules/pareto-ts-validator/data/gitignore ./.gitignore \
+cp ./node_modules/pareto-ts-validator/data/gitignore ../.gitignore \
