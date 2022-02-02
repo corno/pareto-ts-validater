262b23ee5f6b5268422469f84b0c6f69d0624df5
corno <gitkraken.a@schraverus.com>
2022-02-02 08:00:49 +0100

    scripts

--- scripts/buildPubAndTestPackages.sh
+++ scripts/buildPubAndTestPackages.sh
@@ -2,10 +2,10 @@
 
 scriptDir=`realpath $(dirname "$0")`
 
-$scriptDir/buildPackage.sh $(pwd)/pub && \
+$scriptDir/buildPackage.sh $(pwd)/../pub && \
 
 #only execute these 2 if the build was successful
 {
-    find ./pub/dist/esc/bin/* ./pub/dist/bin/* -name "*.js" -exec chmod 777 {} + 2> /dev/null
-    $scriptDir/buildPackage.sh $(pwd)/test
+    find ../pub/dist/esc/bin/* ../pub/dist/bin/* -name "*.js" -exec chmod 777 {} + 2> /dev/null
+    $scriptDir/buildPackage.sh $(pwd)/../test
 }
--- scripts/update2latest.sh
+++ scripts/update2latest.sh
@@ -2,10 +2,10 @@
 
 dir=`realpath $(dirname "$0")`
 
-$dir/updatePackage.sh ./pub
-$dir/updatePackage.sh ./test
+$dir/updatePackage.sh ../pub
+$dir/updatePackage.sh ../test
 
 if [ -d "../dev" ]
 then
-    $dir/updatePackage.sh "./dev"
+    $dir/updatePackage.sh "../dev"
 fi
