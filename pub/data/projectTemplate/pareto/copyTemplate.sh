9320629a140985407fb7f8c5121501563f680e41
corno <gitkraken.a@schraverus.com>
2022-02-02 08:51:13 +0100

    fix for gitignore

--- pub/data/projectTemplate/pareto/copyTemplate.sh
+++ pub/data/projectTemplate/pareto/copyTemplate.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 cp -R ./node_modules/pareto-ts-validator/data/projectTemplate/. .. \
+
+#npm messes with .gitignore, that's why I need to handle it separately
+cp ./node_modules/pareto-ts-validator/data/gitignore ./.gitignore \
