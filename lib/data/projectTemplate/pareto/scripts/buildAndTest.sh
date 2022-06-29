9475e0b282c5bb261729e017779bc0d58f873627
corno <corno.astn.a@schraverus.com>
2022-06-29 07:56:08 +0200

    .

--- lib/data/projectTemplate/pareto/scripts/buildAndTest.sh
+++ lib/data/projectTemplate/pareto/scripts/buildAndTest.sh
@@ -10,4 +10,8 @@ fi \
 
 
 "$(dirname "$0")"/buildPubAndTestPackages.sh && \
-node ../test/dist/esc/bin/index.js ../test/data
+if [ -d "../test" ]
+then
+    node ../test/dist/esc/bin/index.js ../test/data
+fi \
+
