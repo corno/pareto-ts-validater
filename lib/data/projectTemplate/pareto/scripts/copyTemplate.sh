0b5b19f9a415a8c2c3978527c39c38aafca8bd15
corno <corno.astn.a@schraverus.com>
2022-06-29 21:30:19 +0200

    .

--- lib/data/projectTemplate/pareto/scripts/copyTemplate.sh
+++ lib/data/projectTemplate/pareto/scripts/copyTemplate.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 dir=`realpath $(dirname "$0")`
-cp -R ./node_modules/pareto-ts-validator/data/projectTemplate/. .. \
+cp -R ./node_modules/pareto-ts-validator-lib/data/projectTemplate/. .. \
 
 #npm messes with .gitignore, that's why I need to handle it separately
-cp ./node_modules/pareto-ts-validator/data/gitignore ../.gitignore \
+cp ./node_modules/pareto-ts-validator-lib/data/gitignore ../.gitignore \
 
 parts=("lib" "api" "bin" "test")
 for part in "${parts[@]}"
 do
     if [ -d ../$part ]
     then
-        cp ./node_modules/pareto-ts-validator/data/tsconfig.json ../$part
-        cp ./node_modules/pareto-ts-validator/data/_globals.ts ../$part/src
+        cp ./node_modules/pareto-ts-validator-lib/data/tsconfig.json ../$part
+        cp ./node_modules/pareto-ts-validator-lib/data/_globals.ts ../$part/src
     fi
 
 done
 
-#update this package because the copied package.json contains an old version of pareto-ts-validator (by design, the template is not updated every time the package is published)
+#update this package because the copied package.json contains an old version of pareto-ts-validator-lib (by design, the template is not updated every time the package is published)
 $dir/updatePackage.sh .
