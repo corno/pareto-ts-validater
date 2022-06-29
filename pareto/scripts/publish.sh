dea608f7a997ac7db551d6a94d0047ad03a95bca
corno <corno.astn.a@schraverus.com>
2022-06-29 20:44:55 +0200

    .

--- pareto/scripts/publish.sh
+++ pareto/scripts/publish.sh
@@ -8,7 +8,7 @@ git push && \
 git diff --exit-code && \
 
 #make sure latest packages are installed
-$dir/updatePackage.sh ../$2 && \
+#$dir/updatePackage.sh ../$2 && \
 
 #validate that everything is committed
 git diff --exit-code && \
