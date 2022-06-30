8268254aa8e77c08b048352e9a9b98ccfe1e4d8a
corno <corno.astn.a@schraverus.com>
2022-06-30 15:12:18 +0200

    .

--- test/src/esc/bin/index.ts
+++ test/src/esc/bin/index.ts
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import * as pr from "pareto-lang-lib"
+import * as pl from "pareto-lang-lib"
 import * as pt from "pareto-test-lib"
-console.log("no tests yet")
+pl.logDebugMessage("no tests yet")
